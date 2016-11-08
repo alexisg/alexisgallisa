03a253a82e1f802058e1669b87c97e7c2e99775a
Alexis Gallisa <alexis@alexisgallisa.com>
2016-11-07 22:51:27 -0800

    Move portfolio grid to main nav system

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,16 @@
+// We commonly need to grab the url where we are or came from for our animation scenes
+// By default this will return a string enter- + the url path without domain
+var getEnterExitString = function(e) {
+  var url = e + '-' + $(location).attr('pathname').replace(/\//g, '');
+  console.log(url)
+  return url;
+}
+
+
 // On Document Ready
 $(function() {
+
+  $('#js-main').attr('data-enter', getEnterExitString('enter'));
 
   // Video loader if you are on not on a touch device
   var videoLoad = function() {
@@ -17,15 +28,18 @@ $(function() {
   if (!Modernizr.touch) {
     'use strict';
     var $body = $('html, body'),
-    content = $('#js-main').smoothState({
+
+    smoothState = $('#js-main').smoothState({
 
       //smoothState options
       prefetch: true,
       pageCacheSize: 8,
+      blacklist: ".no-smoothstate, [target], [data-type='image'] a",
 
       onStart: {
-        duration: 600,
+        duration: 1000,
         render: function($container) {
+          $('#js-main').attr('data-exit', getEnterExitString('exit'));
           // Scroll page back up
           $body.animate({ scrollTop: 0 });
           // Set classes for animation fading
@@ -35,14 +49,19 @@ $(function() {
         }
       },
 
-      onAfter: function($container, $newContent) {
+      // OnBefore
+      onBefore: function($currentTarget, $content) {
+        // Create a data attribute called exit- which lets the next scene know where it came from.
+        // Get the string value from getEnterExitString
+        // $('#js-main').attr('data-exit', getEnterExitString('exit'));
+      },
 
+      onAfter: function($container, $newContent) {
         // Add the transition start class for transitions duh
         $('#js-main')
           .removeClass('transition-end')
-          .addClass('transition-start');
-        // On new page load, re-initialize the external url anchor function
-        anchorBlank();
+          .addClass('transition-start')
+          .attr('data-enter', getEnterExitString('enter'));
         videoLoad();
       }
     }).data('smoothState');
